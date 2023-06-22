46e944b409d0698e64f989e5903d827d0730917d
Eli Wood <mreliwood1@gmail.com>
2023-06-21 20:06:15 -0700

    updating token expiration to one day

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -41,7 +41,7 @@ router.post('/login', async (req, res) => {
 		const passwordMatch = await bcrypt.compare(req.body.password, user.password);
 		if (!passwordMatch) return res.status(400).json({ error: 'Login failed.' });
 
-		const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: 24 * 60 * 60 });
+		const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '1d' });
 		return res.status(200).json({ token, user });
 	} catch (error) {
 		return res.status(500).json({ error });
